fb8469a314974273a37c8a8bfd0493a63cfa8ede
Marcin <marcingladkowski@gmail.com>
2019-09-15 23:32:37 +0200

    (feat) Tournament fixtures - add dates

--- src/AppBundle/DataFixtures/ORM/TournamentFixtures.php
+++ src/AppBundle/DataFixtures/ORM/TournamentFixtures.php
@@ -2,43 +2,37 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-
 use AppBundle\DataFixtures\BaseFixture;
 use AppBundle\Entity\Place;
 use AppBundle\Entity\Tournament;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-
 class TournamentFixtures extends BaseFixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-
         foreach (range(1,10) as $i) {
             $tournament = new Tournament();
-
-
             $tournament->setName('Turniej ' . $i);
             $tournament->setStart(new \DateTime());
-            $tournament->setStart(new \DateTime());
-
-            $tournament->setPlace($this->getReference(Place::class . '_' . $i));
-
-
+            $tournament->setEnd(new \DateTime());
+            $tournament->setSignUpTill(new \DateTime());
+            $tournament->setCapacity(rand(10, 100));
+            /** @var Place $place */
+            $place = $this->getReference(Place::class . '_' . $i);
+            $tournament->setPlace($place);
             $manager->persist($tournament);
 
             $this->addReference(Tournament::class . '_' . $i, $tournament);
-
         }
-
         $manager->flush();
     }
 
     public function getDependencies()
     {
-        return array(
+        return [
             PlaceFixtures::class,
-        );
+        ];
     }
 }
